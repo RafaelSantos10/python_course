9d45b74c6df34a8ede7da01ee54e10396180b022
Rafael Santos <raffasatos10@gmail.com>
2023-06-01 10:01:09 -0300

    Creating the AccountPoupanca class

--- rafa_bank/conta.py
+++ rafa_bank/conta.py
@@ -17,3 +17,23 @@ class Conta(ABC):
 
     def detalhes(self, msg=""):
         print(f"Seu saldo é {self.saldo:.2f} {msg}")
+
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+
+        if valor_pos_saque >= 0:
+            self.saldo -= valor
+            self.detalhes(f"(SAQUE: {valor})")
+            return self.saldo
+
+        print("Não foi possível sacar o valor desejado")
+        self.detalhes(f"(SAQUE NEGADO: {valor})")
+
+
+if __name__ == "__main__":
+    cp1 = ContaPoupanca(111, 222, 0)
+    cp1.sacar(1)
+    cp1.depositar(1)
+    cp1.sacar(1)
