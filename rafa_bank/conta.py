3a5ce3eceee7986b626b285d41c6139cd627ee63
Rafael Santos <raffasatos10@gmail.com>
2023-06-02 16:25:26 -0300

    Creating the ContaCorrente class

--- rafa_bank/conta.py
+++ rafa_bank/conta.py
@@ -1,22 +1,24 @@
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractmethod
 
 
 class Conta(ABC):
-    def __init__(self, agencia, conta, saldo=0):
+    def __init__(self, agencia: int, conta: int, saldo: float = 0) -> None:
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
 
-    @abstractclassmethod
-    def sacar(self, valor):
+    @abstractmethod
+    def sacar(self, valor: float) -> float:
         ...
 
-    def depositar(self, valor):
+    def depositar(self, valor: float) -> float:
         self.saldo += valor
-        self.detalhes(f"(DEPÓSITO: {valor})")
+        self.detalhes(f"(DEPÓSITO {valor})")
+        return self.saldo
 
-    def detalhes(self, msg=""):
-        print(f"Seu saldo é {self.saldo:.2f} {msg}")
+    def detalhes(self, msg: str = "") -> None:
+        print(f"O seu saldo é {self.saldo:.2f} {msg}")
+        print("--")
 
 
 class ContaPoupanca(Conta):
@@ -25,29 +27,32 @@ class ContaPoupanca(Conta):
 
         if valor_pos_saque >= 0:
             self.saldo -= valor
-            self.detalhes(f"(SAQUE: {valor})")
+            self.detalhes(f"(SAQUE {valor})")
             return self.saldo
 
         print("Não foi possível sacar o valor desejado")
-        self.detalhes(f"(SAQUE NEGADO: {valor})")
+        self.detalhes(f"(SAQUE NEGADO {valor})")
+        return self.saldo
 
 
 class ContaCorrente(Conta):
-    def __init__(self, agencia, conta, saldo=0, limite=0):
+    def __init__(self, agencia: int, conta: int, saldo: float = 0, limite=0):
         super().__init__(agencia, conta, saldo)
         self.limite = limite
 
-    def sacar(self, valor):
+    def sacar(self, valor: float) -> float:
         valor_pos_saque = self.saldo - valor
         limite_maximo = -self.limite
 
         if valor_pos_saque >= limite_maximo:
             self.saldo -= valor
-            self.detalhes(f"(SAQUE: {valor:.2f})")
+            self.detalhes(f"(SAQUE {valor})")
             return self.saldo
 
         print("Não foi possível sacar o valor desejado")
-        self.detalhes(f"(SAQUE NEGADO: {valor})")
+        print(f"Seu limite é {-self.limite:.2f}")
+        self.detalhes(f"(SAQUE NEGADO {valor})")
+        return self.saldo
 
 
 if __name__ == "__main__":
@@ -55,7 +60,13 @@ if __name__ == "__main__":
     # cp1.sacar(1)
     # cp1.depositar(1)
     # cp1.sacar(1)
+    # cp1.sacar(1)
+    print("##")
     cc1 = ContaCorrente(111, 222, 0, 100)
-    cc1.sacar(1)
-    cc1.depositar(1)
-    cc1.sacar(1)
+    # cc1.sacar(1)
+    # cc1.depositar(1)
+    # cc1.sacar(1)
+    # cc1.sacar(1)
+    # cc1.sacar(98)
+
+    print("##")
