b6f3aedcaad1b2ef857cbd34cdac2d93e43b5318
Rafael Santos <raffasatos10@gmail.com>
2023-06-04 11:14:09 -0300

    Relational Databases with Python

---
+++ base_dados_relacional/main.py
@@ -0,0 +1,11 @@
+"""
+Entidades - Tabelas
+
+Tem atributos - colunas
+Primary Key - PK
+    Identificador de cada registro na tabela,valor de chave é único, não se
+    repete, não pode ser nulo, tem que haver apenas uma primary key po tabela.
+Foreign Key - FK
+    A coluna que possui uma FK referência outra coluna em outra tabela, e
+    geralmente a FK passa a ser a PK na outra tabela
+"""
